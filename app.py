8dd9f58d841375e14908350e38c78198871cdf1b
rshajgude <102157144+rshajgude@users.noreply.github.com>
2025-09-07 22:15:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ rev_word_index={value:key for key, value in word_index.items()}
 
 
 # load model in h5 file
-model=tensorflow.keras.models.load_model('simple_rnn_imdb.h5')
+model=tensorflow.keras.models.load_model('./simple_rnn_imdb.h5')
 
 # 2 functions to convert text to vector so model can use another function for reversing vector to text
 
@@ -56,3 +56,4 @@ if st.button('Classify'):
 else:
 
     st.write('Please enter review')
+
